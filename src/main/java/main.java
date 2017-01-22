29afc64993c362291a036ce97908766b84ddf9f1
phost1111 <phost1111@gmail.com>
2017-01-22 14:49:11 +0100

    Removed Tokens for security

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -13,9 +13,9 @@ public class main {
 
         ClientManager.setDeveloperMode(true);
         if(!ClientManager.getDeveloperMode())
-            ClientManager.newClientInstance("MjcyMzgxODI4MTA5ODI4MDk3.C2UK-Q.IAIm-WPqzVkiS9Ux3C3sDgwrPhU", true);
+            ClientManager.newClientInstance("INSERT TOKEN HERE", true);
         else
-            ClientManager.newClientInstance("MjIyMDU4MzcxOTk2NTgxODkw.C2UMgA.H7WmR72Uf_z4hEc12o1otpOt78M", true);
+            ClientManager.newClientInstance("INSERT TEST BOT TOKEN HERE", true);
         ClientManager.newDispatcher();
         ClientManager.getDispatcher().registerListener(new ReadyEventListener());
         ClientManager.getDispatcher().registerListener(new MessageReceivedEventListener());
