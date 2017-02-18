54ba15785c42cb4df2cbe08840e61084127da5e5
phost1111 <phost1111@gmail.com>
2017-02-18 17:42:52 +0100

    release 0.3.2a

--- src/main/java/client/ClientManager.java
+++ src/main/java/client/ClientManager.java
@@ -13,7 +13,7 @@ public class ClientManager {
     public static boolean isDeveloperMode;
     public static IDiscordClient theClient;
     public static EventDispatcher theDispatcher;
-    public static String Version = "0.3.2";
+    public static String Version = "0.3.2a";
 
 
     public static void newClientInstance(String token, Boolean login){
